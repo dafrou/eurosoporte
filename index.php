73f1d4e901aff8f81dfdf516177a17649be4dacf
Christian Meza <christianmezah11@gmail.com>
2014-11-07 16:57:26 -0300

    Creando proyecto

---
+++ index.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <!--[if IE]>
+            <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+            <![endif]-->
+        <title>Euro Soporte</title>
+        <!-- BOOTSTRAP CORE STYLE CSS -->
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="css/index.css" rel="stylesheet" type="text/css"/>
+        <!-- FONTAWESOME STYLE CSS -->
+        <link href="font-awesome-4.2.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <!-- CUSTOM STYLE CSS -->   
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <!-- GOOGLE FONT -->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+
+    </head>
+    <body >
+
+        <div class="navbar navbar-inverse navbar-fixed-top" >
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">YOUR LOGO</a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="index.html">INICIO</a></li>
+                        <li><a href="equipo.html">NUESTRO EQUIPO</a></li>
+                        <li><a href="procediminetos.html">PROCEDIMIENTOS</a></li>
+                        <li><a href="descargas.html">DESCARGAS</a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+        <!--/.NAVBAR END-->
+
+        <section id="home" class="text-center">
+
+            <div id="carousel-example" class="carousel slide" data-ride="carousel">
+
+                <div class="carousel-inner">
+                    <div class="item active">
+
+                        <img src="imagenes/carrusel/slider1.jpg" alt="" />                       
+                        <div class="carousel-caption" >
+                            <h4 class="back-light">INFORMATICA TI EUROCORP</h4>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="imagenes/carrusel/slider2.jpg" alt="" />
+                        <div class="carousel-caption ">
+                            <h4 class="back-light">Aenean faucibus luctus enim. Duis quis sem risu suspend lacinia elementum nunc.</h4>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="imagenes/carrusel/slider3.jpg" alt="" />
+                        <div class="carousel-caption ">
+                            <h4 class="back-light">Aenean faucibus luctus enim. Duis quis sem risu suspend lacinia elementum nunc.</h4>
+                        </div>
+                    </div>
+                </div>
+
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel-example" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-example" data-slide-to="1"></li>
+                    <li data-target="#carousel-example" data-slide-to="2"></li>
+                </ol>
+            </div>
+
+        </section>
+        <!--/.SLIDESHOW END-->
+
+
+        <section id="just-intro">
+            <div class="container">
+                <div class="row text-center pad-row">
+                    <div class="col-md-4  col-sm-4">
+                        <i class="fa fa-gears fa-5x"></i>
+                        <h4> <strong>Acceso a Euroges</strong> </h4>
+                        <p>
+                            Ingresa a nuestro portal de soporte, en este podras
+                            crear tus solicitudes, las cuales seran atendidas 
+                            por nuestros tecnicos.
+                        </p>
+                        <a href="http://www.euroges.cl" class="btn btn-warning" >Acceder</a>    
+                    </div>
+                    <div class="col-md-4  col-sm-4">
+                        <i class="fa fa-envelope  fa-5x"></i>
+                        <h4> <strong>Mail corporativo</strong> </h4>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Curabitur nec nisl odio. Mauris vehicula at nunc id posuere.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        </p>
+                        <a href="#" class="btn btn-warning" >Acceder</a>
+                    </div>
+                    <div class="col-md-4  col-sm-4">
+                        <i class="fa fa-users  fa-5x"></i>
+                        <h4> <strong>Intranet</strong> </h4>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Curabitur nec nisl odio. Mauris vehicula at nunc id posuere.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        </p>
+                        <a href="#" class="btn btn-warning" >Acceder</a>    
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+        <section id="just-intro">
+            <div class="container">
+                <div class="row text-center pad-row">
+                    <div class="col-md-4  col-sm-4">
+                        <i class="fa fa-support fa-5x"></i>
+                        <h4> <strong>Portal de Soporte</strong> </h4>
+                        <p>
+                            Ingresa a nuestro portal de soporte, en este podras
+                            crear tus solicitudes, las cuales seran atendidas 
+                            por nuestros tecnicos.
+                        </p>
+                        <a href="http://www.euroconstructora.cl:8080" class="btn btn-warning" >Acceder</a>    
+                    </div>
+                    <div class="col-md-4  col-sm-4">
+                        <i class="fa fa-mobile  fa-5x"></i>
+                        <h4> <strong>Plan Funcionario</strong> </h4>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Curabitur nec nisl odio. Mauris vehicula at nunc id posuere.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        </p>
+                        <a href="#" class="btn btn-warning" >Acceder</a>
+                    </div>
+                    <div class="col-md-4  col-sm-4">
+                        <i class="fa fa-laptop  fa-5x"></i>
+                        <h4> <strong>Inventario de equipos</strong> </h4>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Curabitur nec nisl odio. Mauris vehicula at nunc id posuere.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        </p>
+                        <a href="#" class="btn btn-warning" >Acceder</a>    
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <section id="offer"  >
+            <div class="container">
+                <div class="row   alert alert-info" >
+                    <div class="col-md-8 col-sm-8">
+                        <h1>Descarga de software corporativo</h1>
+                    </div>
+                    <div class="col-md-4 col-sm-4" style="padding-top: 15px;">
+                        <a href="#" class=" btn btn-warning btn-lg">Descargar</a> 
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <!--/.JUST-INTRO END-->
+        <section  class="note-sec" >
+
+            <div class="container">
+                <div class="row text-center pad-row" >
+                    <div class="col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 ">
+                        <i class="fa fa-quote-left fa-3x"></i>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Curabitur nec nisl odio. Mauris vehicula at nunc id posuere.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Curabitur nec nisl odio. Mauris vehicula at nunc id posuere.
+                        </p>
+                    </div>
+                </div>
+            </div>   
+
+        </section>
+        <!--/.NOTE END-->
+        <section id="clients"  >
+
+
+            <div class="container">
+                <div class="row text-center pad-bottom" >
+                    <div class="col-md-12">       
+                        <img src="imagenes/index/empresas.jpg" width="100%" class="img-responsive"/>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <!--/.CLIENTS END-->
+        <section id="footer-sec" >
+
+            <div class="container">
+                <div class="row  pad-bottom" >
+                    <div class="col-md-4">
+                        <h4> <strong>SOBRE NUESTRA EMPRESA</strong> </h4>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Curabitur nec nisl odio. Mauris vehicula at nunc id posuere.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        </p>
+                        <a href="#" >Mas Sobre Eurocorp</a>
+                    </div>
+                    <div class="col-md-4">
+                        <h4> <strong>SOCIAL LINKS</strong> </h4>
+                        <p>
+                            <a href="#"><i class="fa fa-facebook-square fa-3x"  ></i></a>  
+                            <a href="#"><i class="fa fa-twitter-square fa-3x"  ></i></a>  
+                            <a href="#"><i class="fa fa-linkedin-square fa-3x"  ></i></a>  
+                            <a href="#"><i class="fa fa-google-plus-square fa-3x"  ></i></a>  
+                        </p>
+                    </div>
+                    <div class="col-md-4">
+                        <h4> <strong>Nuestra Ubicacion</strong> </h4>
+                        <p>
+                            Av El Bosque Norte 0177 <br />
+                            Las Condes  <br />
+                          
+                        </p>
+                        <a href="https://www.google.cl/maps/place/El+Bosque+Nte+177,+Las+Condes,+Regi%C3%B3n+Metropolitana/@-33.4151832,-70.6012388,17z/data=!3m1!4b1!4m2!3m1!1s0x9662cf41c998c107:0xf8927547ab64e020" class="btn btn-primary" >Como llegar</a>
+                    </div>
+                </div>
+            </div>
+        </section>         
+        <!--/.FOOTER END-->
+        <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+        <!-- CORE JQUERY  -->
+        <script src="js/jquery-1.10.2..js" type="text/javascript"></script>
+        <!-- BOOTSTRAP SCRIPTS  -->
+        <script src="js/bootstrap.js" type="text/javascript"></script>
+        <!-- CUSTOM SCRIPTS  -->
+        <script src="js/custom.js" type="text/javascript"></script>
+        <script>
+            (function (i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                        m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+            ga('create', 'UA-48795421-1', 'binarytheme.com');
+            ga('send', 'pageview');
+
+        </script>
+    </body>
+</html>
